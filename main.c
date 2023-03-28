242ef33a084ae691848676b83d2243f19ac0b1ab
sadruka <sadrukaafrin4@gmail.com>
2023-03-28 11:17:44 +0600

    nested loop

---
+++ main.c
@@ -0,0 +1,17 @@
+//printing 1-100 using nested loop
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int i,j,n;
+
+for(i=1; i<=10; i++){
+        n=i;
+    for(j=1; j<=10; j++,n=n+10){
+        printf("%d ",n);
+    }
+    printf("\n");
+}
+    return 0;
+}
